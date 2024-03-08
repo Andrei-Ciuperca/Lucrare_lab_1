7073e71e6897f7256364498a6153ba55111672f4
Andrei Ciupercă <ciuperca.andries@gmail.com>
2024-03-08 11:45:08 +0200

    Created new project
    Project intends to simulate GIT functionality

---
+++ Lucrare 3/src/FolderMonitor.java
@@ -0,0 +1,16 @@
+import java.text.SimpleDateFormat;
+
+public class FolderMonitor {
+private String Folder_Path = "Z:\\catalin\\OOP\\Lucrari OOP\\Lucrari_OOP\\Lucrare 3\\gitSimulator";
+private SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+private long lastSnapshotTime;
+
+// we need a way to store what files already exist in the folder and monitor for their changes
+    /**
+     * Storing all existent files in a text file, and print them
+     */
+    private static void createSnapshot(){
+        
+    }
+
+}
---
+++ Lucrare 3/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Folder location: Z:\catalin\OOP\Lucrari OOP\Lucrari_OOP\Lucrare 3\gitSimulator
+        System.out.println("Hello world!");
+    }
+}
